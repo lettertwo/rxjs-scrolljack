bfdbb6992c54402b94931d239e06ec13e408db7c
Eric Eldredge <lettertwo@gmail.com>
2016-08-06 17:35:34 -0400

    Add Wheel observable, subscriber

---
+++ src/Wheel.js
@@ -0,0 +1,78 @@
+import $$observable from 'symbol-observable'
+import {Observable} from 'rxjs/Observable'
+import {WheelSubscriber} from './WheelSubscriber'
+import {fromHijackedEvent} from './operators'
+
+const WHEEL = 'wheel'
+
+export class Wheel extends Observable {
+  constructor (target) {
+    if (typeof target[$$observable] === 'function') {
+      super()
+      this.source = target[$$observable]()
+    } else {
+      super()
+      this.source = fromHijackedEvent(target, WHEEL)
+    }
+  }
+
+  lift (operator) {
+    const observable = new this.constructor(this)
+    observable.operator = operator
+    return observable
+  }
+
+  static from (target) {
+    return new Wheel(target)
+  }
+
+  static start (target) {
+    return new Wheel(target).lift(new WheelStartOperator())
+  }
+
+  static move (target) {
+    return new Wheel(target).lift(new WheelMoveOperator())
+  }
+
+  static stop (target) {
+    return new Wheel(target).lift(new WheelStopOperator())
+  }
+}
+
+export default Wheel
+
+class WheelStartOperator {
+  call (subscriber, source) {
+    return source._subscribe(new WheelStartSubscriber(subscriber))
+  }
+}
+
+class WheelStartSubscriber extends WheelSubscriber {
+  _start (value) {
+    this.dispatch(value)
+  }
+}
+
+class WheelMoveOperator {
+  call (subscriber, source) {
+    return source._subscribe(new WheelMoveSubscriber(subscriber))
+  }
+}
+
+class WheelMoveSubscriber extends WheelSubscriber {
+  _move (value) {
+    this.dispatch(value)
+  }
+}
+
+class WheelStopOperator {
+  call (subscriber, source) {
+    return source._subscribe(new WheelStopSubscriber(subscriber))
+  }
+}
+
+class WheelStopSubscriber extends WheelSubscriber {
+  _stop (value) {
+    this.dispatch(value)
+  }
+}
---
+++ src/WheelSubscriber.js
@@ -0,0 +1,108 @@
+import {Subscriber} from 'rxjs/Subscriber'
+import {async} from 'rxjs/scheduler/async'
+
+const SCROLL_STOP_DELAY = 60
+
+export class WheelSubscriber extends Subscriber {
+  constructor (destination) {
+    super(destination)
+    this._started = false
+    this._ignoring = false
+    this._lastValue
+  }
+
+  _next (value) {
+    const {_lastValue: lastValue} = this
+    this._lastValue = value
+    const {deltaX, deltaY} = value
+    const deltaT = lastValue ? parseInt(value.timeStamp - lastValue.timeStamp, 10) : 0
+
+    if (!this._started) {
+      if (!this._ignoring) {
+        if (deltaT || !lastValue) {
+          this.startNow()
+          this.moveNow()
+          if (deltaX || deltaY) {
+            this.scheduleStop()
+          }
+        } else {
+          this._ignoring = true
+          this.scheduleStop()
+        }
+      } else {
+        if (!deltaX && !deltaY) {
+          this.stopNow()
+        } else {
+          this.scheduleStop()
+        }
+      }
+    } else {
+      if (!deltaX && !deltaY) {
+        this.stopNow()
+      } else {
+        this.moveNow()
+        if (this.scheduled) {
+          this.scheduleStop()
+        }
+      }
+    }
+  }
+
+  _complete () {
+    super._complete()
+    this.cancelStop()
+    this._started = false
+    this._ignoring = false
+    this._lastValue = null
+  }
+
+  cancelStop () {
+    if (this.scheduled) {
+      this.remove(this.scheduled)
+      this.scheduled.unsubscribe()
+      this.scheduled = null
+    }
+  }
+
+  scheduleStop () {
+    this.cancelStop()
+    this.scheduled = async.schedule(dispatchStop, SCROLL_STOP_DELAY, this)
+    this.add(this.scheduled)
+  }
+
+  startNow () {
+    if (!this._started) {
+      this._started = true
+      this._start(this._lastValue)
+    }
+  }
+
+  moveNow () {
+    this._move(this._lastValue)
+  }
+
+  stopNow () {
+    this.cancelStop()
+    if (this._ignoring) {
+      this._ignoring = false
+    }
+    if (this._started) {
+      this._started = false
+      this._stop(this._lastValue)
+    }
+  }
+
+  dispatch (value) {
+    super._next(value)
+  }
+
+  _start () { /* noop */ }
+
+  _move () { /* noop */ }
+
+  _stop () { /* noop */ }
+}
+
+const dispatchStop = subscriber => { subscriber.stopNow() }
+
+export default WheelSubscriber
