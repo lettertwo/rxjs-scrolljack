86448f11d3f9d93e0c3dffa3d2dbf6d52077ef59
Eric Eldredge <lettertwo@gmail.com>
2016-09-07 22:01:08 -0400

    Add velocity to delta

--- src/kinematic/parseOpts.js
+++ src/kinematic/parseOpts.js
@@ -10,6 +10,12 @@ const toDeltaXFromDelta = delta => ({deltaX: delta})
 const toDeltaFromDeltaY = ({deltaY}) => deltaY
 const toDeltaYFromDelta = delta => ({deltaY: delta})
 
+const toVelocityFromVelocityX = ({velocityX}) => velocityX
+const toVelocityXFromVelocity = velocity => ({velocityX: velocity})
+
+const toVelocityFromVelocityY = ({velocityY}) => velocityY
+const toVelocityYFromVelocity = velocity => ({velocityY: velocity})
+
 export const parseOpts = optsLike => {
   if (typeof optsLike.toDelta !== 'function') {
     throw new Error('A `toDelta` option is required.')
@@ -17,6 +23,14 @@ export const parseOpts = optsLike => {
 
   if (typeof optsLike.fromDelta !== 'function') {
     throw new Error('A `fromDelta` option is required.')
+  }
+
+  if (typeof optsLike.toVelocity !== 'function') {
+    throw new Error('A `toVelocity` option is required.')
+  }
+
+  if (typeof optsLike.fromVelocity !== 'function') {
+    throw new Error('A `fromVelocity` option is required.')
   }
 
   return {
@@ -39,11 +53,15 @@ export const parseOptsArray = opts => {
 export const parseXOpts = opts => parseOpts({
   toDelta: toDeltaFromDeltaX,
   fromDelta: toDeltaXFromDelta,
+  toVelocity: toVelocityFromVelocityX,
+  fromVelocity: toVelocityXFromVelocity,
   ...opts,
 })
 
 export const parseYOpts = opts => parseOpts({
   toDelta: toDeltaFromDeltaY,
   fromDelta: toDeltaYFromDelta,
+  toVelocity: toVelocityFromVelocityY,
+  fromVelocity: toVelocityYFromVelocity,
   ...opts,
 })
--- src/operators/DeltaOperator.js
+++ src/operators/DeltaOperator.js
@@ -2,11 +2,44 @@ import {Subscriber} from 'rxjs/Subscriber'
 import {timeStamp} from '../utils'
 
 export class DeltaSubscriber extends Subscriber {
-  constructor (destination, computeDelta) {
+  constructor (destination, computeDelta, computeVelocity) {
     super(destination)
     this.computeDelta = computeDelta || DeltaSubscriber._computeDelta
+    this.computeVelocity = computeVelocity || DeltaSubscriber._computeVelocity
     this._lastValue = null
     this._lastTime = null
+  }
+
+  static _computeVelocity (value, lastValue, deltaT) {
+    let {velocityX, velocityY, deltaX, deltaY} = value
+    let t = deltaT / 1000
+
+    if (value.type && value.type.startsWith('touch')) {
+      if (!lastValue || !lastValue.touches || !lastValue.touches.length) {
+        velocityX = 0
+        velocityY = 0
+      } else {
+        const {clientX: prevX, clientY: prevY} = lastValue.touches[0]
+        const {clientX, clientY} = value.touches[0]
+        velocityX = (prevX - clientX) / t
+        velocityY = (prevY - clientY) / t
+      }
+    } else if (value.type && value.type.startsWith('mouse')) {
+      if (!lastValue) {
+        velocityX = 0
+        velocityY = 0
+      } else {
+        const {clientX: prevX, clientY: prevY} = lastValue
+        const {clientX, clientY} = value
+        velocityX = (prevX - clientX) / t
+        velocityY = (prevY - clientY) / t
+      }
+    } else {
+      velocityX = deltaX / t
+      velocityY = deltaY / t
+    }
+
+    return {velocityX, velocityY}
   }
 
   static _computeDelta (value, lastValue) {
@@ -41,12 +74,13 @@ export class DeltaSubscriber extends Subscriber {
     const {_lastValue: lastValue, _lastTime: lastTime} = this
     const time = timeStamp(value)
     const deltaT = lastTime ? time - lastTime : 0
-    const delta = this.computeDelta(value, lastValue)
+    const delta = this.computeDelta(value, lastValue, deltaT)
+    const velocity = this.computeVelocity(value, lastValue, deltaT)
 
     this._lastValue = value
     this._lastTime = time
 
-    super._next({...delta, deltaT})
+    super._next({...delta, ...velocity, deltaT})
   }
 
   _complete () {
