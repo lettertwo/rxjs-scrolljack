c17bdd36b41166784deb9dd4aebc9781edd1d53f
Eric Eldredge <lettertwo@gmail.com>
2016-09-10 17:49:18 -0400

    Add ScrollBehavior.startWith operator

--- src/ScrollBehavior.js
+++ src/ScrollBehavior.js
@@ -11,21 +11,27 @@ export class ScrollBehavior extends BehaviorSubject {
     this.target = target
     this.Delta = Delta
     this.rect = {...rect}
+
     const {x = 0, y = 0} = this.rect
     this._value = {x, y, ...initialValue}
+
+    const boundsUpdater = bounds(this.rect, Delta.createValue({
+      deltaX: this._value.x,
+      deltaY: this._value.y,
+    }))
 
     if (updater instanceof UpdaterStack) {
       // If the passed in updater is an updater stack,
       // clone it and add a bounds updater.
       this.updater = updater.clone()
-      this.updater.push(bounds(this.rect))
+      this.updater.push(boundsUpdater)
     } else if (updater) {
       // Otherwise, if it's some other kind of updater,
       // make a new updater stack out of the updater plus a bounds updater.
-      this.updater = UpdaterStack.create(updater, bounds(this.rect))
+      this.updater = UpdaterStack.create(updater, boundsUpdater)
     } else {
       // Otherwise, we have no other updaters, so just make a bounds updater.
-      this.updater = bounds(this.rect)
+      this.updater = boundsUpdater
     }
 
     this.source = Delta.move(this.target, this.updater)
@@ -48,6 +54,20 @@ export class ScrollBehavior extends BehaviorSubject {
     // Add the updater we're 'lifting' to the new stack.
     updaterStack.push(updater)
     return new ScrollBehavior(target, Delta, rect, updaterStack, _value)
+  }
+
+  startWith (initialValue) {
+    const {target, Delta, rect, _value} = this
+
+    let updater
+    if (this.updater instanceof UpdaterStack) {
+      updater = this.updater.clone()
+      updater.pop()
+    }
+
+    initialValue = {..._value, ...initialValue}
+
+    return new ScrollBehavior(target, Delta, rect, updater, initialValue)
   }
 
   momentumX (opts) {
