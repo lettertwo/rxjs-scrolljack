4fe3e5c08ca62df892329e7ecf0c4979aedf4a90
Eric Eldredge <lettertwo@gmail.com>
2016-09-11 14:36:13 -0400

    Use default start/stop values in Mouse and Touch

--- src/Mouse.js
+++ src/Mouse.js
@@ -4,20 +4,17 @@ const MOUSE_DOWN = 'mousedown'
 const MOUSE_MOVE = 'mousemove'
 const MOUSE_UP = 'mouseup'
 
-const START_VALUE = Object.freeze({deltaX: 0, deltaY: 0, deltaT: 0})
-const STOP_VALUE = Object.freeze({deltaX: 0, deltaY: 0, deltaT: 0})
-
 export class Mouse extends Delta {
   constructor (target, event = MOUSE_MOVE) {
     super(target, event)
   }
 
-  static start (target, event = MOUSE_DOWN, value = START_VALUE) {
-    return super.start(target, event, value)
+  static start (target, event = MOUSE_DOWN) {
+    return super.start(target, event)
   }
 
-  static stop (target, event = MOUSE_UP, value = STOP_VALUE) {
-    return super.stop(target, event, value)
+  static stop (target, event = MOUSE_UP) {
+    return super.stop(target, event)
   }
 }
 
--- src/Touch.js
+++ src/Touch.js
@@ -4,9 +4,6 @@ const TOUCH_START = 'touchstart'
 const TOUCH_MOVE = 'touchmove'
 const TOUCH_END = 'touchend'
 
-const START_VALUE = Object.freeze({deltaX: 0, deltaY: 0, deltaT: 0})
-const STOP_VALUE = Object.freeze({deltaX: 0, deltaY: 0, deltaT: 0})
-
 const excludeMultiTouch = e => e.touches.length <= 1
 
 export class Touch extends Delta {
@@ -14,12 +11,12 @@ export class Touch extends Delta {
     super(target, event, excludeMultiTouch)
   }
 
-  static start (target, event = TOUCH_START, value = START_VALUE) {
-    return super.start(target, event, value)
+  static start (target, event = TOUCH_START) {
+    return super.start(target, event)
   }
 
-  static stop (target, event = TOUCH_END, value = STOP_VALUE) {
-    return super.stop(target, event, value)
+  static stop (target, event = TOUCH_END) {
+    return super.stop(target, event)
   }
 }
 
