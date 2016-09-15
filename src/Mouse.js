2578d452768b5d605794a24d6961f7468c685e8a
Eric Eldredge <lettertwo@gmail.com>
2016-09-14 20:31:23 -0400

    Delay mouse hijack start until radius is cleared

--- src/Mouse.js
+++ src/Mouse.js
@@ -1,20 +1,38 @@
+import {skipWhile} from 'rxjs/operator/skipWhile'
+import {takeUntil} from 'rxjs/operator/takeUntil'
+import {take} from 'rxjs/operator/take'
+import {mergeStatic as merge} from 'rxjs/operator/merge'
 import {Delta} from './Delta'
+import {inside} from './utils'
 
 const MOUSE_DOWN = 'mousedown'
 const MOUSE_MOVE = 'mousemove'
 const MOUSE_UP = 'mouseup'
+const CLICK = 'click'
+
 
 export class Mouse extends Delta {
   constructor (target, event = MOUSE_MOVE) {
     super(target, event)
   }
 
-  static start (target) {
-    return super.start(target, MOUSE_DOWN)
+  static start (target, radius = {w: 10, h: 10}) {
+    return super
+      .start(target, MOUSE_DOWN)
+      .switchMap(offset => super
+        .create(target)
+        .accumulate()
+        ::takeUntil(this.stop(target))
+        ::skipWhile(netValue => inside(radius.w, radius.h, netValue.deltaX, netValue.deltaY))
+        ::take(1)
+      )
   }
 
   static stop (target) {
-    return super.stop(target, MOUSE_UP)
+    return merge(
+      super.stop(target, MOUSE_UP),
+      super.stop(target, CLICK),
+    )
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,6 @@ export const createWheelEventFrom = (value, type) => {
   const event = new WheelEvent(type, value)
   return event
 }
+
+export const inside = (w, h, dx, dy) => w * h > dx ** 2 + dy ** 2
+export const outside = (w, h, dx, dy) => w * h < dx ** 2 + dy ** 2
