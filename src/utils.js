1bca638b341b7d90bbfd74fbfc43ba2ebea4b12c
Eric Eldredge <lettertwo@gmail.com>
2016-10-04 20:51:20 -0400

    Add createEvent fallback for older browsers

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+/* global WheelEvent */
+
 export const timeStamp = e => e.timeStamp ? parseInt(e.timeStamp, 10) : Date.now()
 
 export const getRoot = () => (
@@ -7,8 +9,33 @@ export const getRoot = () => (
 )
 
 export const createWheelEventFrom = (value, type) => {
-  const event = new WheelEvent(type, value)
+  if (supportsNewEvent()) {
+    return new WheelEvent(type, value)
+  } else {
+    return createOldWheelEventFrom(value, type)
+  }
+}
+
+const createOldWheelEventFrom = (value, type) => {
+  const {view, detail, screenX, screenY, clientX, clientY, button, relatedTarget, deltaX, deltaY, deltaZ, deltaMode} = value
+  const initArgs = [view, detail, screenX, screenY, clientX, clientY, button, relatedTarget, null, deltaX, deltaY, deltaZ, deltaMode]
+  const event = document.createEvent('WheelEvent')
+  event.initWheelEvent(type, true, true, ...initArgs)
   return event
+}
+
+let __supportsNewEvent = null
+
+const supportsNewEvent = () => {
+  if (__supportsNewEvent === null) {
+    try {
+      new WheelEvent('test')  // eslint-disable-line no-new
+      __supportsNewEvent = true
+    } catch (e) {
+      __supportsNewEvent = false
+    }
+  }
+  return __supportsNewEvent
 }
 
 export const inside = (w, h, dx, dy) => w * h > dx ** 2 + dy ** 2
