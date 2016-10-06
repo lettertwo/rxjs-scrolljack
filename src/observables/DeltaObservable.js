1927a35a9ab33f22e61d892c007d87d0159a1564
Eric Eldredge <lettertwo@gmail.com>
2016-10-06 01:10:36 -0400

    Fix DeltaObs static move construction

--- src/observables/DeltaObservable.js
+++ src/observables/DeltaObservable.js
@@ -125,7 +125,7 @@ export class DeltaObservable extends Observable {
   }
 
   static move (target, event) {
-    return new this(target)
+    return new this(target, event)
   }
 
   static moveTo (endValue, updater, scheduler) {
