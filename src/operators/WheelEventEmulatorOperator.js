031b2df999bcd5b86d2f5b27d8c1f709006ce512
Eric Eldredge <lettertwo@gmail.com>
2016-10-31 13:09:02 -0400

    Remove touchpad detection from wheel event operator
    Recent webkit changes have (predictably) rendered this technique
    obsolete. There no longer seems to be any way of reliably detecting
    when wheel events are produced via touchpad.
    
    This is for the better, surely.

--- src/operators/WheelEventEmulatorOperator.js
+++ src/operators/WheelEventEmulatorOperator.js
@@ -1,7 +1,7 @@
 /* global WheelEvent */
 import {Subscriber} from 'rxjs/Subscriber'
 import {async} from 'rxjs/scheduler/async'
-import {timeStamp, supportsNewEvent} from '../utils'
+import {supportsNewEvent} from '../utils'
 import {WHEEL_START, WHEEL_MOVE, WHEEL_END} from '../events'
 
 const SCROLL_STOP_DELAY = 60
@@ -22,65 +22,18 @@ class WheelEventEmulatorSubcriber extends Subscriber {
   constructor (destination) {
     super(destination)
     this._started = false
-    this._ignoring = false
     this._lastValue
-    this._lastTime
   }
 
   _next (value) {
-    const time = timeStamp(value)
-    const {_lastValue: lastValue, _lastTime: lastTime} = this
-    const {deltaX, deltaY} = value
-    const deltaT = lastTime ? time - lastTime : 0
-
     this._lastValue = value
-    this._lastTime = time
 
     if (!this._started) {
-      if (!this._ignoring) {
-        if (deltaT || !lastValue) {
-          // We haven't started yet, and we're not ignoring updates, and we
-          // either have a time delta or haven't updated before, so start
-          // updating now.
-          this.startNow()
-
-          if (deltaX || deltaY) {
-            // We're now started, and we have a delta,
-            // so move on it and schedule a stop timeout.
-            this.moveNow()
-            this.scheduleStop()
-          }
-        } else {
-          // We haven't started yet, but this isn't our first update,
-          // and we don't have a time delta, so start ignoring updates
-          // and schedule a stop timeout.
-          this._ignoring = true
-          this.scheduleStop()
-        }
-      } else {
-        if (!deltaX && !deltaY) {
-          // We haven't started yet, and we're ignoring updates,
-          // and we don't have a delta, so stop now!
-          this.stopNow()
-        } else {
-          // We haven't started yet, and we're ignoring updates,
-          // but we do have a delta, so schedule a stop timeout.
-          this.scheduleStop()
-        }
-      }
+      this.startNow()
+      this.moveNow()
     } else {
-      if (!deltaX && !deltaY) {
-        // We have started, and we don't have a delta, so stop now!
-        this.stopNow()
-      } else {
-        // We have started, and we have a delta, so move on it.
-        this.moveNow()
-        if (this.scheduled) {
-          // We have a stop scheduled, but we just moved on a delta,
-          // so reschedule it for later.
-          this.scheduleStop()
-        }
-      }
+      this.moveNow()
+      this.scheduleStop()
     }
   }
 
@@ -88,22 +41,21 @@ class WheelEventEmulatorSubcriber extends Subscriber {
     super._complete()
     this.cancelStop()
     this._started = false
-    this._ignoring = false
     this._lastValue = null
   }
 
   cancelStop () {
-    if (this.scheduled) {
-      this.remove(this.scheduled)
-      this.scheduled.unsubscribe()
-      this.scheduled = null
+    if (this._scheduled) {
+      this.remove(this._scheduled)
+      this._scheduled.unsubscribe()
+      this._scheduled = null
     }
   }
 
   scheduleStop () {
     this.cancelStop()
-    this.scheduled = async.schedule(dispatchStop, SCROLL_STOP_DELAY, this)
-    this.add(this.scheduled)
+    this._scheduled = async.schedule(dispatchStop, SCROLL_STOP_DELAY, this)
+    this.add(this._scheduled)
   }
 
   startNow () {
@@ -119,9 +71,6 @@ class WheelEventEmulatorSubcriber extends Subscriber {
 
   stopNow () {
     this.cancelStop()
-    if (this._ignoring) {
-      this._ignoring = false
-    }
     if (this._started) {
       this._started = false
       this.dispatch(createWheelEventFrom(this._lastValue, WHEEL_END))
