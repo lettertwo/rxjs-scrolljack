d0ea252f4b3e148079e1cc741024ee0cb0ff04ac
Eric Eldredge <lettertwo@gmail.com>
2016-10-31 23:58:16 -0400

    Throttle scrollStart values by corresponding scrollStops

--- src/Scrolljack.js
+++ src/Scrolljack.js
@@ -1,12 +1,21 @@
 import {map} from 'rxjs/operator/map'
 import {takeUntil} from 'rxjs/operator/takeUntil'
+import {throttle} from 'rxjs/operator/throttle'
 import {DeltaObservable} from './observables/DeltaObservable'
 
 export class Scrolljack extends DeltaObservable {
-  static scrollStart (target, ...DeltaObservableClasses) {
-    return this.from(...DeltaObservableClasses.map(DeltaClass =>
-      DeltaClass.start(target).hijack()
-    ))
+  static scrollStart (target, root, ...DeltaObservableClasses) {
+    const sources = DeltaObservableClasses.map(DeltaClass =>
+      DeltaClass.scrollStart(target).hijack()::throttle(() =>
+        DeltaClass.scrollStop(root).hijack()
+      )
+    )
+
+    if (sources.length > 1) {
+      return this.merge(...sources)
+    } else {
+      return sources[0]
+    }
   }
 
   static scroll (target, root, ...DeltaObservableClasses) {
