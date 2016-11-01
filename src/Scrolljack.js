c140d364967b69c6dc553c8c76290f37ef20998e
Eric Eldredge <lettertwo@gmail.com>
2016-10-31 23:53:40 -0400

    Remove broken Scrolljack.from operator

--- src/Scrolljack.js
+++ src/Scrolljack.js
@@ -1,17 +1,8 @@
-import {mergeStatic as merge} from 'rxjs/operator/merge'
 import {map} from 'rxjs/operator/map'
 import {takeUntil} from 'rxjs/operator/takeUntil'
 import {DeltaObservable} from './observables/DeltaObservable'
 
 export class Scrolljack extends DeltaObservable {
-  static from (...DeltaObservableClasses) {
-    if (DeltaObservableClasses.length > 1) {
-      return new this(merge(...DeltaObservableClasses))
-    } else {
-      return new this(...DeltaObservableClasses)
-    }
-  }
-
   static scrollStart (target, ...DeltaObservableClasses) {
     return this.from(...DeltaObservableClasses.map(DeltaClass =>
       DeltaClass.start(target).hijack()
