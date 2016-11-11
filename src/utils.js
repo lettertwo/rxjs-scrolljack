c75ecc0801acda699e69e6707b0ab76b038c0682
Eric Eldredge <lettertwo@gmail.com>
2016-11-11 15:46:52 -0500

    Add isDeltaLike util

--- src/utils.js
+++ src/utils.js
@@ -12,3 +12,7 @@ export const outside = (w, h, dx, dy) => w * h < dx ** 2 + dy ** 2
 export const hasDelta = value => Boolean(
   value && (value.deltaT || value.deltaX || value.deltaY)
 )
+
+export const isDeltaLike = value => Boolean(
+  value && (value.deltaT != null || value.deltaX != null || value.deltaY != null)
+)
